e1b90f77f27e583bccd693f19847ee5a92690393
Niklas Schönberg <niklas@uplink.fi>
2023-01-24 11:24:16 +0200

    Better build script

--- build.mjs
+++ build.mjs
@@ -13,42 +13,28 @@ const options = {
     write: true,
     sourcemap: true,
     logLevel: 'info',
+    entryNames: '[name]',
     external: ['*.png', '*.svg', '*.jpg', '*.jpeg']
 };
-const jcoreJs = {
-    ...options,
-    entryPoints: getFiles(join(jcorePath, 'js')),
-    loader: {'.js': 'jsx'},
-    outdir: join(jcorePath, 'dist/js'),
-};
+
 const childJs = {
     ...options,
-    entryPoints: getFiles(join(childPath, 'js')),
+    entryPoints: [...getFiles(join(childPath, 'js')), ...getFiles(join(jcorePath, 'js'))],
     loader: {'.js': 'jsx'},
     outdir: join(childPath, 'dist/js'),
 };
-const jcoreSass = {
-    ...options,
-    entryPoints: getFiles(join(jcorePath, 'scss')),
-    plugins: [sassPlugin()],
-    outdir: join(jcorePath, 'dist/css'),
-};
 const childSass = {
     ...options,
-    entryPoints: getFiles(join(childPath, 'scss')),
+    entryPoints: [...getFiles(join(childPath, 'scss')),...getFiles(join(jcorePath, 'scss'))],
     plugins: [sassPlugin()],
     outdir: join(childPath, 'dist/css'),
 };
 
 if (process.argv[2] === 'build') {
-    await esbuild.build(jcoreJs);
     await esbuild.build(childJs);
-    await esbuild.build(jcoreSass);
     await esbuild.build(childSass);
 } else if (process.argv[2] === 'watch') {
-    await (await esbuild.context(jcoreJs)).watch();
     await (await esbuild.context(childJs)).watch();
-    await (await esbuild.context(jcoreSass)).watch();
     await (await esbuild.context(childSass)).watch();
 }
 
@@ -56,6 +42,7 @@ function getFiles(path) {
     const files = [];
     if (existsSync(path)) {
         for (let file of readdirSync(path)) {
+            // Only add files not starting with _ and that has an extension.
             if (file.substring(0, 1) !== '_' && file.split('.').length > 1) {
                 files.push(join(path, file));
             }
