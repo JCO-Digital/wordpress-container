fb81c91a77c0648969fb82e9dde02d33a4a51fec
Niklas Schönberg <niklas@uplink.fi>
2023-02-09 17:26:54 +0200

    New build file

--- build.mjs
+++ build.mjs
@@ -2,9 +2,10 @@
 import * as esbuild from 'esbuild';
 import {sassPlugin} from 'esbuild-sass-plugin';
 import {readdirSync, existsSync} from "fs";
-import {join} from "path";
+import {join, parse} from "path";
 
-const jcorePath = 'wp-content/themes/jcore2';
+
+const jcorePath = join('wp-content/themes', process.env.npm_package_config_parent ?? 'jcore2');
 const childPath = join('wp-content/themes', process.env.npm_package_config_theme ?? 'jcore2-child');
 
 const options = {
@@ -14,18 +15,18 @@ const options = {
     sourcemap: true,
     logLevel: 'info',
     entryNames: '[name]',
-    external: ['*.png', '*.svg', '*.jpg', '*.jpeg']
+    external: ['*.png', '*.svg', '*.jpg', '*.jpeg', '*.css']
 };
 
 const childJs = {
     ...options,
-    entryPoints: [...getFiles(join(childPath, 'js')), ...getFiles(join(jcorePath, 'js'))],
+    entryPoints: deDupe([...getFiles(join(childPath, 'js')), ...getFiles(join(jcorePath, 'js'))]),
     loader: {'.js': 'jsx'},
     outdir: join(childPath, 'dist/js'),
 };
 const childSass = {
     ...options,
-    entryPoints: [...getFiles(join(childPath, 'scss')),...getFiles(join(jcorePath, 'scss'))],
+    entryPoints: deDupe([...getFiles(join(childPath, 'scss')), ...getFiles(join(jcorePath, 'scss'))]),
     plugins: [sassPlugin()],
     outdir: join(childPath, 'dist/css'),
 };
@@ -38,10 +39,16 @@ if (process.argv[2] === 'build') {
     await (await esbuild.context(childSass)).watch();
 }
 
+/**
+ * Get all files that needs to be passed to esbuild from the given path.
+ *
+ * @param path
+ * @returns {*[]}
+ */
 function getFiles(path) {
     const files = [];
     if (existsSync(path)) {
-        for (let file of readdirSync(path)) {
+        for (const file of readdirSync(path)) {
             // Only add files not starting with _ and that has an extension.
             if (file.substring(0, 1) !== '_' && file.split('.').length > 1) {
                 files.push(join(path, file));
@@ -49,4 +56,21 @@ function getFiles(path) {
         }
     }
     return files;
+}
+
+/**
+ * Remove duplicate filenames from list to allow "overwrites".
+ * @param files array<string>
+ * @return array<string>
+ */
+function deDupe(files) {
+    const names = [];
+    return files.filter(file => {
+        const path = parse(file);
+        if (! names.includes(path.name)) {
+            names.push(path.name);
+            return true;
+        }
+        return false;
+    })
 }
