c4ee929b0d7eb3d73b546bd12474648bb6b3aa76
Maxemilian Grönblom <maxemiliang@gmail.com>
2023-06-01 11:21:47 +0300

    feat(mail): Added mailhog container :sparkles:

--- .config/docker-entrypoint.sh
+++ .config/docker-entrypoint.sh
@@ -15,6 +15,7 @@ if [[ ! -f "$WEBROOT/wp-config.php" ]]; then
   echo "Creating wp-config.php"
   echo "wp config create --path=$WEBROOT --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbcharset=$CHARSET --dbprefix=$PREFIX"
   wp config create --path=$WEBROOT --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbcharset=$CHARSET --dbprefix=$PREFIX
+  wp config set --path=$WEBROOT --type=constant JCORE_IS_LOCAL true
   echo "Creating DB."
   wp db create --path=$WEBROOT
   echo "wp db create --path=$WEBROOT"
