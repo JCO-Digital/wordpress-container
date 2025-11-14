fda5c276d5c518226948f4855b62a1c21b502ffb
Johan Österberg <johan.osterberg@jco.fi>
2025-11-14 13:45:27 +0200

    style: Format files according to Prettier configuration.

--- templates/jcore2/build.mjs
+++ templates/jcore2/build.mjs
@@ -1,112 +1,103 @@
 #!/usr/bin/env node
-import * as esbuild from "esbuild";
-import { sassPlugin } from "esbuild-sass-plugin";
-import postcss from "postcss";
-import postcssPresetEnv from "postcss-preset-env";
-import { readdirSync, readFileSync, existsSync } from "node:fs";
-import { join, parse } from "node:path";
-import { parse as tomlParse } from "smol-toml";
+import * as esbuild from 'esbuild';
+import { sassPlugin } from 'esbuild-sass-plugin';
+import postcss from 'postcss';
+import postcssPresetEnv from 'postcss-preset-env';
+import { readdirSync, readFileSync, existsSync } from 'node:fs';
+import { join, parse } from 'node:path';
+import { parse as tomlParse } from 'smol-toml';
 
 const config = {
-    parent: "jcore2",
-    theme: "jcore2-child"
+	parent: 'jcore2',
+	theme: 'jcore2-child',
 };
-if (existsSync("defaults.toml")) {
-  Object.assign(config, tomlParse(readFileSync("defaults.toml", "utf8")));
+if (existsSync('defaults.toml')) {
+	Object.assign(config, tomlParse(readFileSync('defaults.toml', 'utf8')));
 }
-if (existsSync("jcore.toml")) {
-  Object.assign(config, tomlParse(readFileSync("jcore.toml", "utf8")));
+if (existsSync('jcore.toml')) {
+	Object.assign(config, tomlParse(readFileSync('jcore.toml', 'utf8')));
 }
-if (existsSync(".localConfig.toml")) {
-  Object.assign(config, tomlParse(readFileSync(".localConfig.toml", "utf8")));
+if (existsSync('.localConfig.toml')) {
+	Object.assign(config, tomlParse(readFileSync('.localConfig.toml', 'utf8')));
 }
 
-const jcorePath = join("wp-content/themes", config.parent);
-const childPath = join("wp-content/themes", config.theme);
+const jcorePath = join('wp-content/themes', config.parent);
+const childPath = join('wp-content/themes', config.theme);
 
 const buildData = {
-    scripts: [
-        {
-            entryPoints: [join(childPath, "js"), join(jcorePath, "js")],
-            outdir: join(childPath, "/dist/js"),
-        },
-    ],
-    styles: [
-        {
-            entryPoints: [join(childPath, "scss"), join(jcorePath, "scss")],
-            outdir: join(childPath, "/dist/css"),
-        },
-    ],
+	scripts: [
+		{
+			entryPoints: [join(childPath, 'js'), join(jcorePath, 'js')],
+			outdir: join(childPath, '/dist/js'),
+		},
+	],
+	styles: [
+		{
+			entryPoints: [join(childPath, 'scss'), join(jcorePath, 'scss')],
+			outdir: join(childPath, '/dist/css'),
+		},
+	],
 };
-if (existsSync("build.toml")) {
-    Object.assign(buildData, tomlParse(readFileSync("build.toml", "utf8")));
+if (existsSync('build.toml')) {
+	Object.assign(buildData, tomlParse(readFileSync('build.toml', 'utf8')));
 }
 
 const options = {
-  bundle: true,
-  minify: true,
-  write: true,
-  sourcemap: true,
-  logLevel: "info",
-  entryNames: "[name]",
-  external: [
-    "*.png",
-    "*.svg",
-    "*.jpg",
-    "*.jpeg",
-    "*.css",
-    "*.woff",
-    "*.woff2",
-    "*.otf",
-  ],
+	bundle: true,
+	minify: true,
+	write: true,
+	sourcemap: true,
+	logLevel: 'info',
+	entryNames: '[name]',
+	external: ['*.png', '*.svg', '*.jpg', '*.jpeg', '*.css', '*.woff', '*.woff2', '*.otf'],
 };
 
 const sassOptions = {
-  async transform(source, resolveDir) {
-    const { css } = await postcss([
-      postcssPresetEnv({ stage: 2 }),
-    ]).process(source, { from: join(childPath, "dist/css") });
-    return css;
-  },
+	async transform(source, resolveDir) {
+		const { css } = await postcss([postcssPresetEnv({ stage: 2 })]).process(source, {
+			from: join(childPath, 'dist/css'),
+		});
+		return css;
+	},
 };
 
 // Scripts
 for (const script of buildData.scripts) {
-  const entries = [];
-  for (const entry of script.entryPoints) {
-    entries.push(...getFiles(entry));
-  }
-  const config = {
-    ...options,
-    entryPoints: deDupe(entries),
-    loader: { ".js": "jsx" },
-    outdir: script.outdir,
-  };
-  await build(config);
+	const entries = [];
+	for (const entry of script.entryPoints) {
+		entries.push(...getFiles(entry));
+	}
+	const config = {
+		...options,
+		entryPoints: deDupe(entries),
+		loader: { '.js': 'jsx' },
+		outdir: script.outdir,
+	};
+	await build(config);
 }
 
 // Styles
 for (const style of buildData.styles) {
-  const entries = [];
-  for (const entry of style.entryPoints) {
-    entries.push(...getFiles(entry));
-  }
-  const config = {
-    ...options,
-    entryPoints: deDupe(entries),
-    plugins: [sassPlugin(sassOptions)],
-    outdir: style.outdir,
-  };
-  await build(config);
+	const entries = [];
+	for (const entry of style.entryPoints) {
+		entries.push(...getFiles(entry));
+	}
+	const config = {
+		...options,
+		entryPoints: deDupe(entries),
+		plugins: [sassPlugin(sassOptions)],
+		outdir: style.outdir,
+	};
+	await build(config);
 }
 
 async function build(config) {
-  if (process.argv[2] === "build") {
-    await esbuild.build(config);
-  } else if (process.argv[2] === "watch") {
-    // ESbuild 0.17 syntax.
-    await (await esbuild.context(config)).watch();
-  }
+	if (process.argv[2] === 'build') {
+		await esbuild.build(config);
+	} else if (process.argv[2] === 'watch') {
+		// ESbuild 0.17 syntax.
+		await (await esbuild.context(config)).watch();
+	}
 }
 
 /**
@@ -116,16 +107,16 @@ async function build(config) {
  * @returns {*[]}
  */
 function getFiles(path) {
-  const files = [];
-  if (existsSync(path)) {
-    for (const file of readdirSync(path)) {
-      // Only add files not starting with _ and that has an extension.
-      if (file.substring(0, 1) !== "_" && file.split(".").length > 1) {
-        files.push(join(path, file));
-      }
-    }
-  }
-  return files;
+	const files = [];
+	if (existsSync(path)) {
+		for (const file of readdirSync(path)) {
+			// Only add files not starting with _ and that has an extension.
+			if (file.substring(0, 1) !== '_' && file.split('.').length > 1) {
+				files.push(join(path, file));
+			}
+		}
+	}
+	return files;
 }
 
 /**
@@ -134,13 +125,13 @@ function getFiles(path) {
  * @return array<string>
  */
 function deDupe(files) {
-  const names = [];
-  return files.filter((file) => {
-    const path = parse(file);
-    if (!names.includes(path.name)) {
-      names.push(path.name);
-      return true;
-    }
-    return false;
-  });
+	const names = [];
+	return files.filter((file) => {
+		const path = parse(file);
+		if (!names.includes(path.name)) {
+			names.push(path.name);
+			return true;
+		}
+		return false;
+	});
 }
