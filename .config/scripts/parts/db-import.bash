e7fdabe533617262fd9ae5ecda8fd9c13b6925aa
Niklas Schönberg <niklas@uplink.fi>
2025-12-12 12:18:22 +0200

    fix: Add --network flag to wp search-replace command to make it work for multisites as well.

--- .config/scripts/parts/db-import.bash
+++ .config/scripts/parts/db-import.bash
@@ -14,7 +14,7 @@ if [ -f "$SQLPATH/update.sql" ]; then
     record=(${replaceRecord//|/ })
     if [[ ${#record[*]} -ge 2 ]]; then
       echo "Replace ${record[0]} with ${record[1]}"
-      wp search-replace --path="$WEBROOT" --skip-plugins --skip-themes "${record[0]}" "${record[1]}"  --recurse-objects --report-changed-only --skip-columns=guid
+      wp search-replace --network --path="$WEBROOT" --skip-plugins --skip-themes "${record[0]}" "${record[1]}"  --recurse-objects --report-changed-only --skip-columns=guid
     fi
   done
 fi
